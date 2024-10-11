2fa497d61d2e1c3a910b6667b3faeb8de869f02a
yuyi1005 <yuyi1005@foxmail.com>
2024-10-11 15:32:42 +0800

    First upload

---
+++ mmrotate/models/backbones/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .point2rbox_v2_resnet import Point2RBoxV2ResNet
+from .re_resnet import ReResNet
+
+__all__ = ['Point2RBoxV2ResNet', 'ReResNet']
---
+++ mmrotate/models/losses/point2rbox_v2_loss.py
@@ -0,0 +1,422 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import cv2
+import numpy as np
+from mmdet.models.losses.utils import weighted_loss
+
+from mmrotate.registry import MODELS
+from mmrotate.models.losses.gaussian_dist_loss import gwd_loss
+
+
+def bhattacharyya_coefficient(pred, target):
+    """Calculate bhattacharyya coefficient between 2-D Gaussian distributions.
+
+    Args:
+        pred (Tuple): tuple of (xy, sigma).
+            xy (torch.Tensor): center point of 2-D Gaussian distribution
+                with shape (N, 2).
+            sigma (torch.Tensor): covariance matrix of 2-D Gaussian distribution
+                with shape (N, 2, 2).
+        target (Tuple): tuple of (xy, sigma).
+
+    Returns:
+        coef (Tensor): bhattacharyya coefficient with shape (N,).
+    """
+    xy_p, Sigma_p = pred
+    xy_t, Sigma_t = target
+
+    _shape = xy_p.shape
+
+    xy_p = xy_p.reshape(-1, 2)
+    xy_t = xy_t.reshape(-1, 2)
+    Sigma_p = Sigma_p.reshape(-1, 2, 2)
+    Sigma_t = Sigma_t.reshape(-1, 2, 2)
+
+    Sigma_M = (Sigma_p + Sigma_t) / 2
+    dxy = (xy_p - xy_t).unsqueeze(-1)
+    t0 = torch.exp(-0.125 * dxy.permute(0, 2, 1).bmm(torch.linalg.solve(Sigma_M, dxy)))
+    t1 = (Sigma_p.det() * Sigma_t.det()).clamp(1e-7).sqrt()
+    t2 = Sigma_M.det()
+
+    coef = t0 * (t1 / t2).clamp(1e-7).sqrt()[..., None, None]
+    coef = coef.reshape(_shape[:-1])
+    return coef
+
+
+@weighted_loss
+def gaussian_overlap_loss(pred, target, alpha=0.1, beta=0.6065):
+    """Calculate Gaussian overlap loss based on bhattacharyya coefficient.
+
+    Args:
+        pred (Tuple): tuple of (xy, sigma).
+            xy (torch.Tensor): center point of 2-D Gaussian distribution
+                with shape (N, 2).
+            sigma (torch.Tensor): covariance matrix of 2-D Gaussian distribution
+                with shape (N, 2, 2).
+
+    Returns:
+        loss (Tensor): overlap loss with shape (N, N).
+    """
+    mu, sigma = pred
+    B = mu.shape[0]
+    mu0 = mu[None].expand(B, B, 2)
+    sigma0 = sigma[None].expand(B, B, 2, 2)
+    mu1 = mu[:, None].expand(B, B, 2)
+    sigma1 = sigma[:, None].expand(B, B, 2, 2)
+    loss = bhattacharyya_coefficient((mu0, sigma0), (mu1, sigma1))
+    loss[torch.eye(B, dtype=bool)] = 0
+    loss = F.leaky_relu(loss - beta, negative_slope=alpha) + beta * alpha
+    return loss
+
+
+@MODELS.register_module()
+class GaussianOverlapLoss(nn.Module):
+    """Gaussian Overlap Loss.
+
+    Args:
+        reduction (str, optional): The method used to reduce the loss into
+            a scalar. Defaults to 'mean'. Options are "none", "mean" and
+            "sum".
+        loss_weight (float, optional): Weight of loss. Defaults to 1.0.
+
+    Returns:
+        loss (torch.Tensor)
+    """
+
+    def __init__(self,
+                 reduction='mean',
+                 loss_weight=1.0):
+        super(GaussianOverlapLoss, self).__init__()
+        self.reduction = reduction
+        self.loss_weight = loss_weight
+
+    def forward(self,
+                pred,
+                weight=None,
+                avg_factor=None,
+                reduction_override=None):
+        """Forward function.
+
+        Args:
+            pred (Tuple): tuple of (xy, sigma).
+                xy (torch.Tensor): center point of 2-D Gaussian distribution
+                    with shape (N, 2).
+                sigma (torch.Tensor): covariance matrix of 2-D Gaussian distribution
+                    with shape (N, 2, 2).
+            weight (torch.Tensor, optional): The weight of loss for each
+                prediction. Defaults to None.
+            avg_factor (int, optional): Average factor that is used to average
+                the loss. Defaults to None.
+            reduction_override (str, optional): The reduction method used to
+                override the original reduction method of the loss.
+                Options are "none", "mean" and "sum".
+
+        Returns:
+            torch.Tensor: The calculated loss
+        """
+        assert reduction_override in (None, 'none', 'mean', 'sum')
+        reduction = (
+            reduction_override if reduction_override else self.reduction)
+        assert len(pred[0]) == len(pred[1])
+
+        return self.loss_weight * gaussian_overlap_loss(
+            pred,
+            None,
+            weight,
+            reduction=reduction,
+            avg_factor=avg_factor)
+
+
+def plot_gaussian_voronoi_watershed(*images):
+    """Plot figures for debug."""
+    import matplotlib.pyplot as plt
+    plt.figure(dpi=300, figsize=(len(images) * 4, 4))
+    plt.tight_layout()
+    fileid = np.random.randint(0, 20)
+    for i in range(len(images)):
+        img = images[i]
+        img = (img - img.min()) / (img.max() - img.min())
+        if img.dim() == 3:
+            img = img.permute(1, 2, 0)
+        img = img.detach().cpu().numpy()
+        plt.subplot(1, len(images), i + 1)
+        plt.imshow(img)
+        plt.xticks([])
+        plt.yticks([])
+    plt.savefig(f'debug/Gaussian-Voronoi-{fileid}.png')
+    plt.close()
+
+
+def gaussian_2d(xy, mu, sigma, normalize=False):
+    dxy = (xy - mu).unsqueeze(-1)
+    t0 = torch.exp(-0.5 * dxy.permute(0, 2, 1).bmm(torch.linalg.solve(sigma, dxy)))
+    if normalize:
+        t0 = t0 / (2 * np.pi * sigma.det().clamp(1e-7).sqrt())
+    return t0
+
+
+def gaussian_voronoi_watershed_loss(mu, sigma, 
+                                    label, image, 
+                                    pos_thres, neg_thres, 
+                                    down_sample=2, topk=0.95, 
+                                    default_sigma=4096,
+                                    voronoi='gaussian-orientation'):
+    J = len(sigma)
+    if J == 0:
+        return sigma.sum()
+    
+    D = down_sample
+    H, W = image.shape[-2:]
+    h, w = H // D, W // D
+    x = torch.linspace(0, h, h, device=mu.device)
+    y = torch.linspace(0, w, w, device=mu.device)
+    xy = torch.stack(torch.meshgrid(x, y, indexing='xy'), -1)
+    vor = mu.new_zeros(J, h, w)
+    # Get distribution for each instance
+    mm = (mu.detach() / D).round()
+    if voronoi == 'standard':
+        sg = sigma.new_tensor((default_sigma, 0, 0, default_sigma)).reshape(2, 2)
+        sg = sg / D ** 2
+        for j, m in enumerate(mm):
+            vor[j] = gaussian_2d(xy.view(-1, 2), m[None], sg[None]).view(h, w)
+    elif voronoi == 'gaussian-orientation':
+        L, V = torch.linalg.eigh(sigma)
+        L = L.detach().clone()
+        L = L / (L[:, 0:1] * L[:, 1:2]).sqrt() * default_sigma
+        sg = V.matmul(torch.diag_embed(L)).matmul(V.permute(0, 2, 1)).detach()
+        sg = sg / D ** 2
+        for j, (m, s) in enumerate(zip(mm, sg)):
+            vor[j] = gaussian_2d(xy.view(-1, 2), m[None], s[None]).view(h, w)
+    elif voronoi == 'gaussian-full':
+        sg = sigma.detach() / D ** 2
+        for j, (m, s) in enumerate(zip(mm, sg)):
+            vor[j] = gaussian_2d(xy.view(-1, 2), m[None], s[None]).view(h, w)
+    # val: max prob, vor: belong to which instance, cls: belong to which class
+    val, vor = torch.max(vor, 0)
+    if D > 1:
+        vor = vor[:, None, :, None].expand(-1, D, -1, D).reshape(H, W)
+        val = F.interpolate(
+            val[None, None], (H, W), mode='bilinear', align_corners=True)[0, 0]
+    cls = label[vor]
+    kernel = val.new_ones((1, 1, 3, 3))
+    kernel[0, 0, 1, 1] = -8
+    ridges = torch.conv2d(vor[None].float(), kernel, padding=1)[0] != 0
+    vor += 1
+    pos_thres = val.new_tensor(pos_thres)
+    neg_thres = val.new_tensor(neg_thres)
+    vor[val < pos_thres[cls]] = 0
+    vor[val < neg_thres[cls]] = J + 1
+    vor[ridges] = J + 1
+
+    cls_bg = torch.where(vor == J + 1, 15, cls)
+    cls_bg = torch.where(vor == 0, -1, cls_bg)
+
+    # PyTorch does not support watershed, use cv2
+    img_uint8 = (image - image.min()) / (image.max() - image.min()) * 255
+    img_uint8 = img_uint8.permute(1, 2, 0).detach().cpu().numpy().astype(np.uint8)
+    img_uint8 = cv2.medianBlur(img_uint8, 3)
+    markers = vor.detach().cpu().numpy().astype(np.int32)
+    markers = vor.new_tensor(cv2.watershed(img_uint8, markers))
+
+    # if np.random.rand() < 0.002:
+    #     plot_gaussian_voronoi_watershed(image, cls_bg, markers)
+
+    L, V = torch.linalg.eigh(sigma)
+    L_target = []
+    for j in range(J):
+        xy = (markers == j + 1).nonzero()[:, (1, 0)].float()
+        if len(xy) == 0:
+            L_target.append(L[j].detach())
+            continue
+        xy = xy - mu[j]
+        xy = V[j].T.matmul(xy[:, :, None])[:, :, 0]
+        max_x = torch.max(torch.abs(xy[:, 0]))
+        max_y = torch.max(torch.abs(xy[:, 1]))
+        L_target.append(torch.stack((max_x ** 2, max_y ** 2)))
+    L_target = torch.stack(L_target)
+    L = torch.diag_embed(L)
+    L_target = torch.diag_embed(L_target)
+    loss = gwd_loss((None, L), (None, L_target.detach()), reduction='none')
+    loss = torch.topk(loss, int(np.ceil(len(loss) * topk)), largest=False)[0].mean()
+    return loss, (vor, markers)
+
+
+@MODELS.register_module()
+class GaussianVoronoiLoss(nn.Module):
+    """Gaussian Overlap Loss.
+
+    Args:
+        reduction (str, optional): The method used to reduce the loss into
+            a scalar. Defaults to 'mean'. Options are "none", "mean" and
+            "sum".
+        loss_weight (float, optional): Weight of loss. Defaults to 1.0.
+
+    Returns:
+        loss (torch.Tensor)
+    """
+
+    def __init__(self,
+                 down_sample=2,
+                 reduction='mean',
+                 loss_weight=1.0):
+        super(GaussianVoronoiLoss, self).__init__()
+        self.down_sample = down_sample
+        self.reduction = reduction
+        self.loss_weight = loss_weight
+
+    def forward(self, pred, label, image, pos_thres, neg_thres, voronoi='orientation'):
+        """Forward function.
+
+        Args:
+            pred (Tuple): Tuple of (xy, sigma).
+                xy (torch.Tensor): Center point of 2-D Gaussian distribution
+                    with shape (N, 2).
+                sigma (torch.Tensor): Covariance matrix of 2-D Gaussian distribution
+                    with shape (N, 2, 2).
+            image (torch.Tensor): The image for watershed with shape (3, H, W).
+            standard_voronoi (bool, optional): Use standard or Gaussian voronoi.
+
+        Returns:
+            torch.Tensor: The calculated loss
+        """
+        loss, self.vis = gaussian_voronoi_watershed_loss(*pred, 
+                                               label,
+                                               image, 
+                                               pos_thres, 
+                                               neg_thres, 
+                                               self.down_sample, 
+                                               topk=0.95,
+                                               voronoi=voronoi)
+        return self.loss_weight * loss
+
+
+def rbbox2roi(bbox_list):
+    """Convert a list of bboxes to roi format.
+
+    Args:
+        bbox_list (list[Tensor]): a list of bboxes corresponding to a batch
+            of images.
+
+    Returns:
+        Tensor: shape (n, 6), [batch_ind, cx, cy, w, h, a]
+    """
+    rois_list = []
+    for img_id, bboxes in enumerate(bbox_list):
+        if bboxes.size(0) > 0:
+            img_inds = bboxes.new_full((bboxes.size(0), 1), img_id)
+            rois = torch.cat([img_inds, bboxes[:, :5]], dim=-1)
+        else:
+            rois = bboxes.new_zeros((0, 6))
+        rois_list.append(rois)
+    rois = torch.cat(rois_list, 0)
+    return rois
+
+
+def plot_edge_map(feat, edgex, edgey):
+    """Plot figures for debug."""
+    import matplotlib.pyplot as plt
+    plt.figure(dpi=300, figsize=(4, 4))
+    plt.tight_layout()
+    fileid = np.random.randint(0, 20)
+    for i in range(len(feat)):
+        img0 = feat[i, :3]
+        img0 = (img0 - img0.min()) / (img0.max() - img0.min())
+        img1 = edgex[i, :3]
+        img1 = (img1 - img1.min()) / (img1.max() - img1.min())
+        img2 = edgey[i, :3]
+        img2 = (img2 - img2.min()) / (img2.max() - img2.min())
+        img3 = img1 + img2
+        img3 = (img3 - img3.min()) / (img3.max() - img3.min())
+        img = torch.cat((torch.cat((img0, img1), -1), 
+                         torch.cat((img2, img3), -1)), -2
+                         ).permute(1, 2, 0).detach().cpu().numpy()
+        N = int(np.ceil(np.sqrt(len(feat))))
+        plt.subplot(N, N, i + 1)
+        plt.imshow(img)
+        plt.xticks([])
+        plt.yticks([])
+    plt.savefig(f'debug/Edge-Map-{fileid}.png')
+    plt.close()
+
+
+@MODELS.register_module()
+class EdgeLoss(nn.Module):
+    """Edge Loss.
+
+    Args:
+        reduction (str, optional): The method used to reduce the loss into
+            a scalar. Defaults to 'mean'. Options are "none", "mean" and
+            "sum".
+        loss_weight (float, optional): Weight of loss. Defaults to 1.0.
+
+    Returns:
+        loss (torch.Tensor)
+    """
+
+    def __init__(self,
+                 resolution=24,
+                 max_scale=1.6,
+                 sigma=6,
+                 reduction='mean',
+                 loss_weight=1.0):
+        super(EdgeLoss, self).__init__()
+        self.resolution = resolution
+        self.max_scale = max_scale
+        self.sigma = sigma
+        self.reduction = reduction
+        self.loss_weight = loss_weight
+        self.center_idx = self.resolution / self.max_scale
+
+        self.roi_extractor = MODELS.build(dict(
+            type='RotatedSingleRoIExtractor',
+                roi_layer=dict(
+                    type='RoIAlignRotated',
+                    out_size=(2 * self.resolution + 1),
+                    sample_num=2,
+                    clockwise=True),
+            out_channels=1,
+            featmap_strides=[1],
+            finest_scale=1024))
+
+        edge_idx = torch.arange(0, self.resolution + 1)
+        edge_distribution = torch.exp(-((edge_idx - self.center_idx) ** 2) / (2 * self.sigma ** 2))
+        edge_distribution[0] = edge_distribution[-1] = 0
+        self.register_buffer('edge_idx', edge_idx)
+        self.register_buffer('edge_distribution', edge_distribution)
+
+    def forward(self, pred, edge):
+        """Forward function.
+
+        Args:
+            pred (Tuple): Batched predicted rboxes
+            edge (torch.Tensor): The edge map with shape (B, 1, H, W).
+
+        Returns:
+            torch.Tensor: The calculated loss
+        """
+        G = self.resolution
+        C = self.center_idx
+        roi = rbbox2roi(pred)
+        roi[:, 3:5] *= self.max_scale
+        feat = self.roi_extractor([edge], roi)
+        if len(feat) == 0:
+            return pred[0].new_tensor(0)
+        featx = feat.sum(1).abs().sum(1)
+        featy = feat.sum(1).abs().sum(2)
+        featx2 = torch.flip(featx[:, :G + 1], (-1,)) + featx[:, G:]
+        featy2 = torch.flip(featy[:, :G + 1], (-1,)) + featy[:, G:]  # (N, 25)
+        ex = ((featx2 * self.edge_distribution).softmax(1) * self.edge_idx).sum(1) / C
+        ey = ((featy2 * self.edge_distribution).softmax(1) * self.edge_idx).sum(1) / C
+        exy = torch.stack((ex, ey), -1)
+        rbbox_concat = torch.cat(pred, 0)
+        
+        # if np.random.rand() < 0.005:
+        #     edgex = featx[:, None, None, :].expand(-1, 1, 2 * self.resolution + 1, -1)
+        #     edgey = featy[:, None, :, None].expand(-1, 1, -1, 2 * self.resolution + 1)
+        #     plot_edge_map(feat, edgex, edgey)
+
+        return self.loss_weight * F.smooth_l1_loss(rbbox_concat[:, 2:4], 
+                                      (rbbox_concat[:, 2:4] * exy).detach(),
+                                      beta=8)
